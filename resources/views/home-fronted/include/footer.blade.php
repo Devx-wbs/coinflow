b3c996ebaa2af75169f0e6de3902b93a25d82c73
WBS <kirandeepkaur@webblazesofttech.com>
2026-02-13 20:24:52 +0530

    implement design as per figma front landing page

--- resources/views/home-fronted/include/footer.blade.php
+++ resources/views/home-fronted/include/footer.blade.php
@@ -1,59 +1,41 @@
-<footer>
-      <div class="container">
-        <div class="footer-contains">
-          <div class="footer-logo footer-divide">
-            <div class="f-logo">
-              <img src="images/footer_logo.svg" alt="" />
-            </div>
-            <div class="f-logo-text">
-              <p>
-                CoinFlow empowers merchants to accept cryptocurrency payments
-                securely and instantly across the globe
-              </p>
-            </div>
-          </div>
-          <div class="footer-quick-links footer-divide">
-            <h3>Quick Links</h3>
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Pricing</a></li>
-              <li><a href="#">Features</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
-          </div>
-          <div class="footer-resources-links footer-divide">
-            <h3>Resources</h3>
-            <ul>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Help Center</a></li>
-              <li><a href="#">API Reference</a></li>
-              <li><a href="#">Terms of Service</a></li>
-            </ul>
-          </div>
-          <div class="footer-social-links footer-divide">
-            <h3>Follow Us</h3>
-            <ul>
-              <li>
-                <a href="#"><img src="images/twitter.svg" alt="" /></a>
-              </li>
-              <li>
-                <a href="#"><img src="images/insta.svg" alt="" /></a>
-              </li>
-              <li>
-                <a href="#"><img src="images/facebook.svg" alt="" /></a>
-              </li>
-              <li>
-                <a href="#"><img src="images/yt.svg" alt="" /></a>
-              </li>
-            </ul>
-          </div>
+<footer class="footer">
+    <div class="container">
+      <div class="footer-top">
+        <div class="footer-brand">
+          <img src="{{ asset('images/footer_logo.png') }}" alt="CoinFlow" class="footer-logo" />
+          <p class="footer-description">
+            CoinFlow empowers merchants to accept cryptocurrency payments securely and instantly across the globe
+          </p>
         </div>
-        <div class="footer-copyright">
-          <p>Copyright © 2025 CoinFlow. All rights reserved.</p>
+        <div class="footer-links">
+          <h4>Quick Links</h4>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#pricing">Pricing</a></li>
+            <li><a href="#features">Features</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+        <div class="footer-links">
+          <h4>Resources</h4>
+          <ul>
+            <li><a href="#faq">FAQ</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+            <li><a href="#">Terms of Service</a></li>
+          </ul>
+        </div>
+        <div class="footer-social">
+          <h4>Follow Us</h4>
+          <div class="social-icons">
+            <a href="#"><img src="{{ asset('images/soc_a.png') }}" alt="Social" /></a>
+            <a href="#"><img src="{{ asset('images/soc_b.png') }}" alt="Social" /></a>
+            <a href="#"><img src="{{ asset('images/soc_c.png') }}" alt="Social" /></a>
+            <a href="#"><img src="{{ asset('images/soc_d.png') }}" alt="Social" /></a>
+          </div>
         </div>
       </div>
-    </footer>
-
-<script src="{{ asset('script.js') }}"></script>
-
-</html>
+      <div class="footer-bottom">
+        <p>&copy; 2026 CoinFlow. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
