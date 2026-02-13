d8642908f3f50e6e5fba77c2a284c018d671d1fe
WBS <kirandeepkaur@webblazesofttech.com>
2026-02-13 15:57:01 +0530

    login to exact url

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -94,7 +94,7 @@ class LoginController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/login');
+       return redirect()->route('login');
     }
 
 
